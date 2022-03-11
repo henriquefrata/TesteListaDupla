7b389e2bc3c4cbc356472f401ccf085e2bb5df39
Henrique Frata <henrique.frata360@gmail.com>
2022-03-11 12:21:19 -0300

    primeira alteração

---
+++ TesteListaDupla/src/Teste.java
@@ -0,0 +1,18 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Lista lista = new Lista();
+		
+		lista.inserir("Pedro", 123);
+		lista.inserir("Joao", 234);
+		lista.inserir("Matheus", 345);
+		lista.inserir("Bob", 456);
+		lista.inserir("Marcos", 567);		
+		
+		
+		lista.imprimir();		
+	}
+	
+}
